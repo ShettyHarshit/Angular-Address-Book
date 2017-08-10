ea6ff9ea57048de377083cabf22dd6cf82dd087a
Harshit Shetty <swaharsh@gmail.com>
2017-08-11 02:21:39 +0530

    Add contact functionality through button added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 var app = angular.module('my-app', []);
 
 app.controller("contacts-controller", function($scope) {
-  $scope.name = "world";
+  $scope.lmao = "world";
+  
+
   $scope.records = [
-       {
-            "Name" : "Harshit Shetty",
-            "Phone" : "+91 9768 XXX XXX"
-        },{
-            "Name" : "Amar Pendhari",
-            "Phone" : "+91 9768 XXX XXX"
-        },{
-            "Name" : "Abhijeet Shetty",
-            "Phone" : "+91 9768 XXX XXX"
-        },{
-            "Name" : "Adit Arora",
-            "Phone" : "+91 9768 XXX XXX"
-        }
-    ]
+  {
+    "Name" : "Harshit Shetty",
+    "Phone" : "+91 9768 XXX XXX"
+},{
+    "Name" : "Amar Pendhari",
+    "Phone" : "+91 9768 XXX XXX"
+},{
+    "Name" : "Abhijeet Shetty",
+    "Phone" : "+91 9768 XXX XXX"
+},{
+    "Name" : "Adit Arora",
+    "Phone" : "+91 9768 XXX XXX"
+}
+]
+    
+    $scope.add = function() {
+        $scope.records.push({Name: $scope.input, Phone: $scope.input2});
+        $scope.input = '';
+        $scope.input2 = '';
+    };
 });
