2e9dcbcbd4757b8d7959fade4ea87863536febf1
Harshit Shetty <swaharsh@gmail.com>
2017-08-13 02:24:12 +0530

    Age+Image options added

--- js/index.js
+++ js/index.js
@@ -20,26 +20,37 @@ app.controller("contacts-controller", function($scope) {
   $scope.records = [
   {
     "Name" : "Harshit Shetty",
-    "Phone" : "+91 9768 XXX XXX"
-    "Address" : "Me Kyu batau?"
+    "Age" : "20",
+    "Phone" : "+91 9768 XXX XXX",
+    "Address" : "Me Kyu batau?",
+    "Link" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Nikolaj_Coster-Waldau_by_Gage_Skidmore.jpg/170px-Nikolaj_Coster-Waldau_by_Gage_Skidmore.jpg"
 },{
     "Name" : "Amar Pendhari",
-    "Phone" : "+91 9768 XXX XXX"
-    "Address" : "Me Kyu batau?"
+    "Age" : "20",
+    "Phone" : "+91 9768 XXX XXX",
+    "Address" : "Me Kyu batau?",
+    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
 },{
     "Name" : "Abhijeet Shetty",
-    "Phone" : "+91 9768 XXX XXX"
-    "Address" : "Me Kyu batau?"
+    "Age" : "20",
+    "Phone" : "+91 9768 XXX XXX",
+    "Address" : "Me Kyu batau?",
+    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
 },{
     "Name" : "Adit Arora",
-    "Phone" : "+91 9768 XXX XXX"
-    "Address" : "Me Kyu batau?"
+    "Age" : "20",
+    "Phone" : "+91 9768 XXX XXX",
+    "Address" : "Me Kyu batau?",
+    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
 }
 ]
     
     $scope.add = function() {
-        $scope.records.push({Name: $scope.input, Phone: $scope.input2});
+        $scope.records.push({Name: $scope.input, Phone: $scope.input2, Address: $scope.input3, Age: $scope.input4,  Link: $scope.input5});
         $scope.input = '';
         $scope.input2 = '';
+        $scope.input3 = '';
+        $scope.input4 = '';
+        $scope.input5 = '';
     };
 });
