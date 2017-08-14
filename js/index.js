3ad4ddef0ea5814908e302362902c002fda810fe
Harshit Shetty <swaharsh@gmail.com>
2017-08-15 02:23:50 +0530

    Material cards added

--- js/index.js
+++ js/index.js
@@ -59,25 +59,25 @@ $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved)
     "Age" : "20",
     "Phone" : "+91 9768 XXX XXX",
     "Address" : "Me Kyu batau?",
-    "Link" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Nikolaj_Coster-Waldau_by_Gage_Skidmore.jpg/170px-Nikolaj_Coster-Waldau_by_Gage_Skidmore.jpg"
+    "Link" : "https://media1.popsugar-assets.com/files/thumbor/Dut3rlQY7ZXRivu3TvfmnQmlFtQ/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/02/07/963/n/1922283/b01db4d1_edit_img_image_34626342_1438031640_718/i/Jaime-Lannister-GIFs.jpg"
 },{
     "Name" : "Amar Pendhari",
     "Age" : "20",
     "Phone" : "+91 9768 XXX XXX",
     "Address" : "Me Kyu batau?",
-    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
+    "Link" : "http://www.meckmin.org/wp-content/uploads/2015/08/person-placeholder-608x608.jpg"
 },{
     "Name" : "Abhijeet Shetty",
     "Age" : "20",
     "Phone" : "+91 9768 XXX XXX",
     "Address" : "Me Kyu batau?",
-    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
+    "Link" : "http://www.meckmin.org/wp-content/uploads/2015/08/person-placeholder-608x608.jpg"
 },{
     "Name" : "Adit Arora",
     "Age" : "20",
     "Phone" : "+91 9768 XXX XXX",
     "Address" : "Me Kyu batau?",
-    "Link" : "http://style.anu.edu.au/_anu/4/images/placeholders/person.png"
+    "Link" : "http://www.meckmin.org/wp-content/uploads/2015/08/person-placeholder-608x608.jpg"
 }
 ];
 localStorage.setItem('todos', JSON.stringify($scope.todos));
